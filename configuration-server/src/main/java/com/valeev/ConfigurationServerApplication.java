f916a1206c0a21bac763149d740b516eb4ac7ced
Arthur <webster_91@inbox.ru>
2020-09-03 22:35:01 +0300

    Добавил конфиг сервер

---
+++ configuration-server/src/main/java/com/valeev/ConfigurationServerApplication.java
@@ -0,0 +1,17 @@
+package com.valeev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigurationServerApplication {
+    public ConfigurationServerApplication() {
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationServerApplication.class, args);
+    }
+
+}
