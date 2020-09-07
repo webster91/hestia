fdf0a6872591122f565c9f1f4c0e4159a1cbdb5e
Arthur <webster_91@inbox.ru>
2020-09-08 01:31:39 +0300

    Добавил дисковери сервер

--- app/src/main/java/com/valeev/hestia/HestiaApplication.java
+++ app/src/main/java/com/valeev/hestia/HestiaApplication.java
@@ -3,10 +3,12 @@ package com.valeev.hestia;
 import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableEurekaClient
 @EnableMongock
 public class HestiaApplication {
 
